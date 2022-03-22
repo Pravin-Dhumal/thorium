12302cfb1a94fcb997822f255935e03dbc893083
pathakradhika02 <pathakradhika393@gmail.com>
2022-03-22 20:52:44 +0530

    project intern

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -37,55 +37,6 @@ const createCollege = async function( req , res ) {
 
 
 
-
-
-// const getColleges = async function ( req ,res ) {
-//     try {
-//         const colName = req.query.collegeName
-
-//         // check : if colNmae is empty
-//         if( !isValid(colName) )    return res.status(400).send({ status : false, message: 'please provide college Name'})
-        
-//         // check : if any college is exist with given name 
-//         const collegeId = await collegeModel.find({ name : colName }).select({ _id : 1})
-//         if( !Object.keys(collegeId).length > 0)  return res.status(404).send({ status : false, message: 'No data found'})
-
-//         // retrieve : college
-//         let colleges = await collegeModel.findById(collegeId).select({ name:1 , fullname:1 , logoLink:1 , _id:0})
-
-// //          let colleges = await collegeModel.find({ name : colName}).select({ name:1 , fullname:1 , logoLink:1 , _id:0})
-
-//         // retrieve : interested interns
-//         const intern = await internModel.find({ collegeId : collegeId })
-
-//         console.log("col>>>>>..",colleges)
-
-//         console.log("--------------------------------------------------------------------------------------------------------------")
-//         console.log("--------------------------------------------------------------------------------------------------------------")
-        
-// console.log("interns     66    ",intern)
-
-// console.log("--------------------------------------------------------------------------------------------------------------")
-// console.log("--------------------------------------------------------------------------------------------------------------")
-
-// colleges.interests = intern
-
-// console.log("collegs ....70    ..",colleges)
-        
-//         res.status(200).send({status : true ,data : colleges })
-//     }
-//     catch ( error ) {
-//         console.log(error.message)
-//         return res.status(500).send({status : false , message : error.message})
-//     }
-// }
-
-
-
-
-
-
-
 const getColleges = async function ( req ,res ) {
     try {
         const colName = req.query.collegeName
