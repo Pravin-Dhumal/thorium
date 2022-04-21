ef6b23f7c16bb4ad5258af9127f064cb75015888
pathakradhika02 <pathakradhika393.com>
2022-04-21 18:13:48 +0530

    product-management

---
+++ src/models/cartModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: ObjectId, 
+        ref: "user", 
+        required: 'userId is required', 
+        unique: true
+    },
+    items: [{
+            _id: 0 ,
+            productId: {
+                type: ObjectId,
+                required: 'productId is required',
+                refs: "product"
+            },
+            quantity: {
+                type: Number,
+                required: "Enter quantity required",
+                min: 1
+            }
+        }] ,
+    totalPrice: {
+        type: Number, 
+        required: true,
+    },
+    totalItems: {
+        type: Number, 
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('cart', cartSchema)
+//let model = mongoose.model('cart', cartSchema)
+
+
+//  hson = like includes
