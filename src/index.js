9a55f8e0b81eb722e694cc3ee42d9d6defc2286a
Pravin-Dhumal <iampravin.dhumal@gmail.com>
2022-05-21 19:55:28 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://pathakradhika02:xwz7d3tIw343fIrl@cluster0.4kdiu.mongodb.net/project-1", {
+mongoose.connect("mongodb+srv://pravindhumal:xwz7d3tIw343fIrl@cluster0.4kdiu.mongodb.net/project-1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
